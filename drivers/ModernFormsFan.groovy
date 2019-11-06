84458b13084c3f6dbf454bacd2f18764412234e9
gjunky <1info@globalexpress.com>
2019-11-05 19:38:16 -0700

    Update ModernFormsFan.groovy
    Version 1.0 with most capabilities included

--- drivers/ModernFormsFan.groovy
+++ drivers/ModernFormsFan.groovy
@@ -3,13 +3,18 @@
 *
 * Allows basic control of the Fan part of Simple Form Fans
 * 
+*    Versions: 1.0    Initial version with most capabilities included
  */
 metadata {
     definition(name: "Simple Form Fan", namespace: "gjunky", author: "RobJodh@gmail.com") {
         capability "FanControl"
-//        command "FanOn"
-//        command "FanOff"
+        capability "SwitchLevel"
         command "GetStatus"
+        command "FanOn"
+        command "FanOff"
+        command "LightOn"
+        command "LightOff"
+        command "Direction", [[name:"Fan Direction", type: "ENUM", description: "Pick an option", constraints: ["forward","reverse","Summer","Winter"] ] ]
     }
 }
 
@@ -62,46 +67,95 @@ def params = [
     } catch (e) {
         log.debug "something went wrong: $e"
         checkHttpResponse("error in $command", e.getResponse())
-		return false
+		return null
     }
 }
-
 /*
-def FanOn() {
-               if (logEnable) log.debug "Sending On Command " + speed
+ * Command Section
+ */
 
-               sendCommand("fanOn", true)
+def FanOn() {
+    if (logEnable) log.debug "Sending Fan On Command " + speed
+
+    sendCommand("fanOn", true)
 }
 
 def FanOff() {
-               if (logEnable) log.debug "Sending Off Command " + speed
+    if (logEnable) log.debug "Sending Fan Off Command " + speed
 
-               sendCommand("fanOn", false)
+    sendCommand("fanOn", false)
 }
-*/
 
+def LightOn() {
+    if (logEnable) log.debug "Sending Light On Command " + speed
+
+    sendCommand("lightOn", true)
+}
+
+def LightOff() {
+    if (logEnable) log.debug "Sending Light Off Command " + speed
+
+    sendCommand("lightOn", false)
+}
+
+def Direction(direction) {
+    if (logEnable) log.debug "Sending Fan Direction Command " + direction
+    if (direction == "Summer") direction="forward"
+    if (direction == "Winter") direction="reverse"
+    sendCommand("fanDirection", "'$direction'")
+    // Fan Direction doesn't seem to correctly report status, calling it manually
+    // GetStatus()
+}
+
+def GetStatus() {
+    if (logEnable) log.debug("Querying Fan")
+    showStatus(sendCommand("queryDynamicShadowData", 1))
+}
+
+
+/*
+ *    Set the fan speed (only 1-5 is supported in the capability). Convert Low-High --> 1-5
+ *        Handle the On and Off "Speed" as on/off commands
+ */
 def setSpeed(speed) {
-               if (logEnable) log.debug "Sending Speed Command " + speed
-               def speedInt = getSpeedValue(speed)
-               if (speedInt > 0) sendCommand("fanSpeed", speedInt)
-               else if (speed == "on") sendCommand("fanOn", true)
-               else if (speed == "off") sendCommand("fanOn", false)
+    if (logEnable) log.debug "Sending Speed Command " + speed
+    def speedInt = getSpeedValue(speed)
+    if (speedInt > 0) sendCommand("fanSpeed", speedInt)
+    else if (speed == "on") sendCommand("fanOn", true)
+        else if (speed == "off") sendCommand("fanOn", false)
 }
 
+/*
+ *    Set the light's brightness
+ */
+def setLevel(level) {
+    if (logEnable) log.debug "Sending Light Level " + level
+    sendCommand("lightBrightness", level)
+}
+
+/*
+ *    Show the current status in the device page
+ */
 def showStatus(retData) {
-    if (logEnable) log.debug("Show Status: $retData")
     if (retData) {
+        if (logEnable) log.debug("Show Status: $retData")
+        def sleepUntil = 0
+        
         if (retData.fanOn) device.sendEvent(name: "fan", value: "On")
         else device.sendEvent(name: "fan", value: "Off")
         device.sendEvent(name: "fanSpeed", value: retData.fanSpeed)
         device.sendEvent(name: "fanDirection", value: retData.fanDirection)
         if (retData.lightOn) device.sendEvent(name: "light", value: "On")
         else device.sendEvent(name: "light", value: "Off")
+        device.sendEvent(name: "lightBrightness", value: retData.lightBrightness)
+        if (retData.fanSleepTimer) {
+            sleepUntil = new Date((retData.fanSleepTimer as long)*1000).format( 'M-d-yyyy HH:mm-ss' )
+            device.sendEvent(name: "fanSleepTimer", value: sleepUntil)
+        }
+        if (retData.fanSleepTimer) {
+            sleepUntil = new Date((retData.lightSleepTimer as long)*1000).format( 'M-d-yyyy HH:mm-ss' )
+            device.sendEvent(name: "lightSleepTimer", value: retData.lightSleepTimer)
+        }
     }
-}
-
-def GetStatus() {
-    if (logEnable) log.debug("Querying Fan")
-    showStatus(sendCommand("queryDynamicShadowData", 1))
 }
 
